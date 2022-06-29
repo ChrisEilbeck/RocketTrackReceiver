f1bf4f38d29632dac3b14d64e1af9fad3af9eae2
Chris Eilbeck <chris@hyperspace.org.uk>
2022-06-29 09:00:38 +0100

    This version has a mechanism to switch between high rate and long range modes if packets are not being received.  This should then flip to the opposite mode and as long as packets come in, stay in that mode.

--- RocketTrackReceiver/RocketTrackReceiver.ino
+++ RocketTrackReceiver/RocketTrackReceiver.ino
@@ -33,12 +33,40 @@ unsigned long UpdateClientAt=0;
 #define LORA_FREQ           434650000
 
 double lora_frequency=LORA_FREQ;
-double lora_offset=0;
-int lora_mode=0;
+double lora_offset=4000;
+int lora_mode=1;
+int last_good_receive=0;
 
 AXP20X_Class axp;
 
 // initialize the library with the numbers of the interface pins
+
+void SetLoRaMode(int mode)
+{
+#if LOW_POWER_TRANSMIT
+	LoRa.setTxPower(5);
+#else
+	LoRa.setTxPower(17);
+#endif
+	
+	switch(mode)
+	{
+		case 0:		Serial.println("Setting LoRa to long range mode");
+					LoRa.setSpreadingFactor(12);
+					LoRa.setSignalBandwidth(31.25E3);
+					LoRa.setCodingRate4(8);
+					break;
+		
+		case 1:		Serial.println("Setting LoRa to high rate mode");
+					LoRa.setSpreadingFactor(7);
+					LoRa.setSignalBandwidth(125E3);
+					LoRa.setCodingRate4(8);
+					break;
+		
+		default:	Serial.println("Duff LoRa mode selected!");
+					break;
+	}
+}
 
 void setup()
 {
@@ -84,19 +112,21 @@ void setup()
 	
 	LoRa.setPins(LORA_NSS,LORA_RESET,LORA_DIO0);
 	
-	if(!LoRa.begin(434.650E6))
+	if(!LoRa.begin(lora_frequency+lora_offset))
 	{
 		Serial.println("Starting LoRa failed!");
 		while (1);
 	}
 	
 	LoRa.receive();
-
-#if 1
+	
+	SetLoRaMode(lora_mode);
+	
+#if 0
 	LoRa.setSpreadingFactor(12);
 	LoRa.setSignalBandwidth(31.25E3);
 	LoRa.setCodingRate4(8);
-	LoRa.setFrequency(lora_frequency);
+	LoRa.setFrequency(lora_frequency+lora_offset);
 #endif
 	
 #endif
@@ -149,13 +179,16 @@ void loop()
 			Serial.print(offset);
 			Serial.println(" Hz");
 			
-			if(offset>100)	lora_offset-=20.0;
-			if(offset<-100)	lora_offset+=200.0;
+			if(offset>100)			lora_offset-=20.0;
+			else if(offset<-100)	lora_offset+=200.0;
+			else					lora_offset+=offset;
 			
 			LoRa.setFrequency(lora_frequency+lora_offset);
 			
 			free(packet);
 		}
+		
+		last_good_receive=millis();
 	}
 	
 	UpdateClient();
@@ -175,18 +208,24 @@ void UpdateClient(void)
  		sprintf(Line,"CurrentRSSI=%d\r\n",CurrentRSSI);
 	#else
 		memset(Line,0,sizeof(Line));
-		
-		int bar;
-		for(bar=0;bar<CurrentRSSI+140;bar++)
-			Line[bar]='#';
-		Line[bar]='\r';
-		Line[bar+1]='\n';
+		memset(Line,'#',CurrentRSSI+135);
+//		sprintf(Line+CurrentRSSI+140,"\r\n");
 	#endif
 		
  		Serial.println(Line);
 #endif
 		
 		UpdateClientAt=millis()+100;
+	}
+	
+	if(		(		(lora_mode==0)
+				&&	(millis()>(last_good_receive+30000))	)
+		||	(		(lora_mode==1)
+				&&	(millis()>(last_good_receive+5000))	)	)
+	{
+		lora_mode=!lora_mode;
+		SetLoRaMode(lora_mode);
+		last_good_receive=millis();
 	}
 }
   
