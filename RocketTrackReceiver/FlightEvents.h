5917aac798ef70fe1372427836515458a94a395f
Chris Eilbeck <chris@hyperspace.org.uk>
2024-09-25 12:36:35 +0100

    Started to add support for basic flight event detection from the received data packets.

---
+++ RocketTrackReceiver/FlightEvents.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+extern bool detect_launch;
+extern bool detect_apogee;
+extern bool detect_lowlevel;
+extern bool detect_landing;
+
---
+++ RocketTrackReceiver/FlightEvents.ino
@@ -0,0 +1,8 @@
+
+#include "FlightEvents.h"
+
+bool detect_launch=false;
+bool detect_apogee=false;
+bool detect_lowlevel=false;
+bool detect_landing=false;
+
